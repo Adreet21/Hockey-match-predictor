5f67f7fef15639182b5e54d48872a5289c80c1fd
SYED SHAHMEER RAHMAN <adreet21@gmail.com>
2024-07-16 14:57:47 +0600

    Update scrapper.py

--- scrapper.py
+++ scrapper.py
@@ -4,6 +4,9 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 YEARS = list(range(2024, 2022, -1))  # 2024, 2023
 MATCHES = []
